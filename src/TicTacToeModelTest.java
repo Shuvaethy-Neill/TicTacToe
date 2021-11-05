d4206fcfd76e6942120c47cbb6353bb0b7835b00
Shuvaethy-Neill <shuvaethy.neill@gmail.com>
2021-11-04 22:53:38 -0400

    finished test class

--- src/TicTacToeModelTest.java
+++ src/TicTacToeModelTest.java
@@ -2,20 +2,76 @@ import static org.junit.Assert.*;
 
 public class TicTacToeModelTest {
 
+    private TicTacToeModel tttm = new TicTacToeModel();
+
     @org.junit.Before
     public void setUp() throws Exception {
     }
 
     @org.junit.Test
     public void getStatusForInitialTicTacToe() {
-        TicTacToeModel tttm = new TicTacToeModel();
         assertEquals(TicTacToeModel.Status.UNDECIDED, tttm.getStatus());
     }
 
     @org.junit.Test
     public void getStatusAfterFirstMove() {
-        TicTacToeModel tttm = new TicTacToeModel();
         tttm.play(1, 1);
         assertEquals(TicTacToeModel.Status.UNDECIDED, tttm.getStatus());
+    }
+
+    @org.junit.Test
+    public void getStatusAfterHorizontalWin() {
+        tttm.play(1, 1);
+        tttm.play(2, 1);
+        tttm.play(1, 0);
+        tttm.play(2, 2);
+        tttm.play(1, 2);
+        assertEquals(TicTacToeModel.Status.X_WON, tttm.getStatus());
+    }
+
+    @org.junit.Test
+    public void getStatusAfterVerticalWin() {
+        tttm.play(0, 1);
+        tttm.play(0, 2);
+        tttm.play(1, 0);
+        tttm.play(1, 2);
+        tttm.play(1, 1);
+        tttm.play(2, 2);
+        assertEquals(TicTacToeModel.Status.O_WON, tttm.getStatus());
+    }
+
+    @org.junit.Test
+    public void getStatusAfterDiagonalLeftWin() {
+        tttm.play(0, 0);
+        tttm.play(2, 0);
+        tttm.play(1, 1);
+        tttm.play(0, 1);
+        tttm.play(2, 2);
+        assertEquals(TicTacToeModel.Status.X_WON, tttm.getStatus());
+    }
+
+    @org.junit.Test
+    public void getStatusAfterDiagonalRightWin() {
+        tttm.play(0, 0);
+        tttm.play(0, 2);
+        tttm.play(0, 1);
+        tttm.play(1, 1);
+        tttm.play(2, 2);
+        tttm.play(2, 0);
+        assertEquals(TicTacToeModel.Status.O_WON, tttm.getStatus());
+    }
+
+    @org.junit.Test
+    public void getStatusAfterTie() {
+        tttm.play(0, 0);
+        tttm.play(0, 1);
+        tttm.play(0, 2);
+        tttm.play(1, 0);
+        tttm.play(1, 1);
+        tttm.play(2, 0);
+        tttm.play(2, 1);
+        tttm.play(1, 2);
+        tttm.play(2, 2);
+        assertEquals(TicTacToeModel.Status.TIE, tttm.getStatus());
     }
 }
